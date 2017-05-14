31d665576222db6cb0ab470ea4c9833f8c9d2c3c
三城聖 <sei.e5hayabusa@gmail.com>
2017-05-14 12:09:41 +0900

    Initial Commit

---
+++ IdeaShare/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  IdeaShare
+//
+//  Created by 三城聖 on 2017/05/14.
+//  Copyright © 2017年 三城聖. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
